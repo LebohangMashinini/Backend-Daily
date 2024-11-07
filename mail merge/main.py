e74a6d22773a059a305e5a3b770f86c1cbddeceb
LebohangMashinini <palesal613@gmail.com>
2024-11-07 12:44:52 +0200

    updated

--- mail merge/main.py
+++ mail merge/main.py
@@ -1,3 +1,29 @@
+def letter_file():
+    with open("./Input/letters/starting_letter.docx", "r") as file:
+        lines = file.readlines()
+        return lines
+
+def names_file():
+    names = []
+    f = open('./Input/Names/inivited_names.txt', 'r')
+    contents = f.readlines()
+    for line in contents:    
+        names.append(line.strip())
+        
+    return names
+
+def construct_letter():
+    names =  names_file()
+    lst = letter_file()
+    letter = "".join(lst)
+    new_letter = ''
+    for name in names:
+      new_letter = letter.replace("[name]",name)
+      
+      with open(f"./Input/ReadyToSend/{name}.docx", "w") as file:
+            file.write(new_letter)
+            
+construct_letter()
 #TODO: Create a letter using starting_letter.docx 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
