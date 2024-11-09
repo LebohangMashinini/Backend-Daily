ea11da88570ae679d94c13fb06758a1b2f8afea5
LebohangMashinini <palesal613@gmail.com>
2024-11-09 14:05:53 +0200

    added error handling method and dunder method

--- mail merge/main.py
+++ mail merge/main.py
@@ -1,29 +1,46 @@
 def letter_file():
-    with open("./Input/letters/starting_letter.docx", "r") as file:
-        lines = file.readlines()
+    try:
+        with open("./Input/letters/starting_letter.docx", "r") as file:
+            lines = file.readlines()
         return lines
-
+    except FileNotFoundError:
+        print("error: starting_letter.docx file not found")
+        return []
 def names_file():
     names = []
-    f = open('./Input/Names/inivited_names.txt', 'r')
-    contents = f.readlines()
-    for line in contents:    
-        names.append(line.strip())
-        
-    return names
+    try:
+        f = open('./Input/Names/inivited_names.txt', 'r')
+        contents = f.readlines()
+        for line in contents:    
+            names.append(line.strip())
+        return names
+    except FileNotFoundError:
+        print("Error: inivited_names.txt file not found")
+        return []
 
 def construct_letter():
     names =  names_file()
-    lst = letter_file()
-    letter = "".join(lst)
+    if not names:
+        print("No names found. Aborting")
+
+    letter_content = letter_file()
+    if not letter_content:
+        print("No letter content found. Aborting")
+
+    letter = "".join(letter_content)
     new_letter = ''
+
     for name in names:
-      new_letter = letter.replace("[name]",name)
-      
-      with open(f"./Input/ReadyToSend/{name}.docx", "w") as file:
-            file.write(new_letter)
+        new_letter = letter.replace("[name]",name)
+        try:
+            with open(f"./Input/ReadyToSend/{name}.docx", "w") as file:
+                file.write(new_letter)
+        except IOError:
+            print("Error: could not create a letter")
             
-construct_letter()
+
+if __name__ == "__main__":
+    construct_letter()
 #TODO: Create a letter using starting_letter.docx 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
